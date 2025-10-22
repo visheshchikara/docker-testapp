5b5c1dab5975015b3ed672c8e0f33822ae2d643c
vish <vish@123>
2025-10-22 10:53:03 +0530

    vishesh commit

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.get("/getUsers", async (req, res) => {
     await client.connect(URL);
     console.log('Connected successfully to server');
 
-    const db = client.db("apnacollege-db");
+    const db = client.db("vishesh-db");
     const data = await db.collection('users').find({}).toArray();
-    
+
     client.close();
     res.send(data);
 });
@@ -29,7 +29,7 @@ app.post("/addUser", async (req, res) => {
     await client.connect(URL);
     console.log('Connected successfully to server');
 
-    const db = client.db("apnacollege-db");
+    const db = client.db("vishesh-db");
     const data = await db.collection('users').insertOne(userObj);
     console.log(data);
     console.log("data inserted in DB");
